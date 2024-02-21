0d089c2b725954b8d8482095ad7fb888f98f2406
KyawZinLin3 <kyawzinlin14future@gmail.com>
2024-02-21 21:00:25 +0630

    learning

--- hangman/src/App.tsx
+++ hangman/src/App.tsx
@@ -1,7 +1,25 @@
+import { useState } from "react";
+import words from "./wordList.json";
+
 function App() {
+  const [wordToGuess, setWordToGuess] = useState(() => {
+    return words[Math.floor(Math.random() * words.length)];
+  });
+  const [guessLetter, setGuessLatter] = useState<string[]>([]);
   return (
     <>
-      <h1>Hi</h1>
+      <div
+        style={{
+          maxWidth: "800px",
+          display: "flex",
+          flexDirection: "column",
+          gap: "2rem",
+          margin: "0 auto",
+          alignItems: "center",
+        }}
+      >
+        <div style={{ fontSize: "2rem", textAlign: "center" }}>Lose Win</div>
+      </div>
     </>
   );
 }

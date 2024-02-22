365dd7600fc2a5f0d5d056a3bc51731689e988e6
KyawZinLin3 <kyawzinlin14future@gmail.com>
2024-02-22 22:38:12 +0630

    learning

--- hangman/src/HangmanWord.tsx
+++ hangman/src/HangmanWord.tsx
@@ -1,7 +1,23 @@
 import React from "react";
 
+const word = "test";
 function HangmanWord() {
-  return <div>HangmanWord</div>;
+  return (
+    <div
+      style={{
+        display: "flex",
+        gap: ".25em",
+        fontSize: "6rem",
+        fontWeight: "bold",
+        textTransform: "uppercase",
+        fontFamily: "monospace",
+      }}
+    >
+      {word.split("").map((lett, index) => (
+        <span style={{ borderBottom: ".1em solid black" }}>{lett}</span>
+      ))}
+    </div>
+  );
 }
 
 export default HangmanWord;
